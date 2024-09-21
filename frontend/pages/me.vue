a454ad72b0845f036423159fa91e48ba24578bb7
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-09-21 19:14:47 -0300

    Added middleware to protect routes

--- frontend/pages/me.vue
+++ frontend/pages/me.vue
@@ -1,9 +1,18 @@
 <script setup>
   import axios from 'axios';
 
+  definePageMeta({
+    middleware: ["auth"]
+  })
+
   const { data: user } = await axios.get("/user");
 </script>
+
 <template>
-  <p><strong>User Name:</strong> {{ user.name }}</p>
-  <p><strong>User Email:</strong> {{ user.email }}</p>
+  <p>
+    <strong>User Name:</strong> {{ user.name }}
+  </p>
+  <p>
+    <strong>User Email:</strong> {{ user.email }}
+    </p>
 </template>
